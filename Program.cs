874b66180a414232b124e9ca8dd055f2d063c128
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-07-15 14:33:57 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB4_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
